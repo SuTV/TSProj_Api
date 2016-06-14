c570283758ae2ab9380fa083bd44f2f7bbf4541b
SuTV <su@planday.com>
2016-06-14 18:43:54 +0700

    impl return secret field with permission

--- common/models/answer.js
+++ common/models/answer.js
@@ -5,7 +5,7 @@ module.exports = function(Answer) {
     // enable some endpoints
     enableAllMethods(Answer, ['find', 'upsert', 'create', 'updateAll', 'count']);
     
-    Answer.observe('before save', function(ctx, next) {        
+    Answer.observe('before save', function(ctx, next) {      
         // update time
         if (ctx.instance) {
             ctx.instance.lastUpdated = new Date();
@@ -16,8 +16,59 @@ module.exports = function(Answer) {
         next();
     });
 
-    Answer.afterRemote('findById', function(ctx, result, next) {
-      result.isCorrectAnswer = result.isCorrect;
-      next();
+    function filterIsCorrectField(ctx, next, userId) {
+        var allowToView = false;
+
+        var currentContext = loopback.getCurrentContext();
+        var currentUser = currentContext && currentContext.get('currentUser');
+        if(currentUser) {
+            allowToView = currentUser.id === userId;
+        }
+
+        if(!allowToView) {
+            if(ctx.instance) {
+                ctx.instance.unsetAttribute('isCorrect');
+            } else {
+                delete ctx.data.isCorrect;
+            }
+        }
+
+        next();
+    };
+
+    Answer.observe('loaded', function(ctx, next) {
+
+        var questionId = ctx.instance ? ctx.instance.questionId : ctx.data.questionId;
+
+        // get question
+        Answer.app.models.Question.findOne({id: questionId, filter: {fields: {id: true, examId: true}}}, function(err, question) {
+            if(err) next(err);
+
+            if(question && question.examId) {
+                // get exam
+                Answer.app.models.Exam.findOne({id: question.examId, filter: {fields: {id: true, courseId: true, lectureId: true}, include: ['course', 'lecture']}}, function(err, exam) {
+                    if(err) next(err);
+
+                    if(exam && exam.courseId && exam.course) {
+                        return filterIsCorrectField(ctx, next, exam.course.userId);
+                    } else if(exam.lectureId && exam.lecture) {
+                        // get plan
+                        Answer.app.models.Plan.findOne({id: exam.lecture.planId, filter: {fields: {id: true, courseId: true}, include: ['course']}}, function(err, plan) {
+                            if(err) next(err);
+
+                            if(plan && plan.courseId && plan.course) {
+                                return filterIsCorrectField(ctx, next, plan.course.userId);
+                            } else {
+                                next(new Error('could not find plan'));
+                            }
+                        });
+                    } else {
+                        next(new Error('could not find exam'));
+                    }
+                });
+            } else {
+                next(new Error('could not find question'));
+            }
+        });
     });
 };
--- common/models/question.js
+++ common/models/question.js
@@ -16,8 +16,49 @@ module.exports = function(Question) {
         next();
     });
 
-    Question.afterRemote('findById', function(ctx, result, next) {
-      result.answerYesOrTrue = result.isAnswerYesOrTrue;
-      next();
+    function filterIsAnswerYesOrTrueField(ctx, next, userId) {
+        var allowToView = false;
+
+        var currentContext = loopback.getCurrentContext();
+        var currentUser = currentContext && currentContext.get('currentUser');
+        if(currentUser) {
+            allowToView = currentUser.id === userId;
+        }
+
+        if(!allowToView) {
+            if(ctx.instance) {
+                ctx.instance.unsetAttribute('isAnswerYesOrTrue');
+            } else {
+                delete ctx.data.isAnswerYesOrTrue;
+            }
+        }
+
+        next();
+    };
+
+    Question.observe('loaded', function(ctx, next) {
+        var examId = ctx.instance ? ctx.instance.examId : ctx.data.examId;
+
+        // get exam
+        Question.app.models.Exam.findOne({id: examId, filter: {fields: {id: true, courseId: true, lectureId: true}, include: ['course', 'lecture']}}, function(err, exam) {
+            if(err) next(err);
+
+            if(exam && exam.courseId && exam.course) {
+                return filterIsAnswerYesOrTrueField(ctx, next, exam.course.userId);
+            } else if(exam.lectureId && exam.lecture) {
+                // get plan
+                Question.app.models.Plan.findOne({id: exam.lecture.planId, filter: {fields: {id: true, courseId: true}, include: ['course']}}, function(err, plan) {
+                    if(err) next(err);
+
+                    if(plan && plan.courseId && plan.course) {
+                        return filterIsAnswerYesOrTrueField(ctx, next, plan.course.userId);
+                    } else {
+                        next(new Error('could not find plan'));
+                    }
+                });
+            } else {
+                next(new Error('could not find exam'));
+            }
+        });        
     });
 };
