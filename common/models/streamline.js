02232f06cb5e7a1f540fe0e21ad24aa6ba0a876e
SuTV <su@planday.com>
2016-07-13 20:05:41 +0700

    add streamline model

---
+++ common/models/streamline.js
@@ -0,0 +1,18 @@
+var loopback = require('loopback');
+var enableAllMethods = require('../utils/model.js').enableAllMethods;
+
+module.exports = function(Streamline) {
+    // disable some endpoints
+    enableAllMethods(Streamline, ['find', 'upsert', 'create', 'updateAll', 'count']);
+    
+    Streamline.observe('before save', function(ctx, next) {
+        // update time
+        if (ctx.instance) {
+            ctx.instance.lastUpdated = new Date();
+        } else {
+            ctx.data.lastUpdated = new Date();
+        }
+        
+        next();
+    });
+};
